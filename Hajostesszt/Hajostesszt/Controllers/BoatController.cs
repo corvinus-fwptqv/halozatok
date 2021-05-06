7eadc9839bf2c52c137fb616962a4eded418f0b4
Levente Gárdonyi <levente.gardonyi@stud.uni-corvinus.hu>
2021-05-06 17:45:44 +0200

    tizedik

--- Hajostesszt/Hajostesszt/Controllers/BoatController.cs
+++ Hajostesszt/Hajostesszt/Controllers/BoatController.cs
@@ -12,14 +12,29 @@ namespace Hajostesszt.Controllers
     [ApiController]
     public class BoatController : ControllerBase
     {
+        
+        [HttpGet]
+        [Route("questions/{sorszám}")]
+        public ActionResult M2(int sorszám)
+        {
+            HajostesztContext context = new HajostesztContext();
+            var kérdés = (from x in context.Questions
+                          where x.QuestionId == sorszám
+                          select x).FirstOrDefault();
+            if (kérdés == null) return BadRequest("Nincs ilyen sorszámú kérdés");
+
+            return new JsonResult(kérdés);
+        }
         [HttpGet]
         [Route("questions/all")]
         public ActionResult M1()
         {
             HajostesztContext context = new HajostesztContext();
             var kérdések = from x in context.Questions select x.QuestionText;
-
             return new JsonResult(kérdések);
         }
     }
+
 }
+
+
